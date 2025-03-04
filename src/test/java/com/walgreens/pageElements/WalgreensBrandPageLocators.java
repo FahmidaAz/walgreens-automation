0dc47f1b5f1b06bb777efca325bb57ded4560917
FahmidaAz <AzFahmida96@gmail.com>
2025-03-04 10:37:06 -0500

    first scenario

---
+++ src/test/java/com/walgreens/pageElements/WalgreensBrandPageLocators.java
@@ -0,0 +1,9 @@
+package com.walgreens.pageElements;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class WalgreensBrandPageLocators {
+        @FindBy(xpath="//input[@id='filter_118']")
+        public WebElement brandCheckbox;
+}
